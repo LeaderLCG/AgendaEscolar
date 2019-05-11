980c15607d0639aa956b5cccdd5268256e195c8c
Enrique Constantino <enriqueconss@gmail.com>
2019-05-11 12:34:12 -0500

    Contactos con recylcer
    Pantalla de contactos con recycler view

---
+++ app/src/main/java/com/example/laion/logintestv2/AdapterDatos.java
@@ -0,0 +1,59 @@
+package com.example.laion.logintestv2;
+
+import android.media.Image;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+
+public class AdapterDatos extends RecyclerView.Adapter<AdapterDatos.ViewHolderDatos> {
+
+    ArrayList<String>  listDatos;
+
+    public AdapterDatos(ArrayList<String> listDatos) {
+        this.listDatos = listDatos;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolderDatos onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.contacts_list,null,false);
+        return new ViewHolderDatos(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull viewHolderDatos viewHolderDatos, int i) {
+        int position;
+        holder.asignarDatos(listDatos.get(position));
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return listDatos.size();
+    }
+
+    public class ViewHolderDatos extends RecyclerView.ViewHolder {
+
+        TextView nombreContacto;
+
+        public ViewHolderDatos(View itemView){
+            super(itemView);
+            nombreContacto=(TextView) itemView.findViewById(R.id.txtNombreContacto);
+
+        }
+
+        public void asignarDatos(String nombreContacto){
+            nombreContacto.setText(nombreContacto);
+        }
+    }
+
+
+}
